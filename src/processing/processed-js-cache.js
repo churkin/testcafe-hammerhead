018f55779ce3dc9f176ea7150be3bf69c6adcb2c
Churkin Andrey <andrey.churkin@devexpress.com>
2015-07-28 15:17:02 +0300

    lru-cache integration

--- src/processing/processed-js-cache.js
+++ src/processing/processed-js-cache.js
@@ -1,64 +1,27 @@
 import Crypto from 'crypto';
-
-//NOTE: Max cache size is 50 MBytes
-const MAX_SIZE = 50 * 1024 * 1024;
-//NOTE: clean up cache to the half of the max size
-const CLEAN_UP_DEST_SIZE = Math.round(MAX_SIZE / 2);
+import Lru from 'lru-cache';
 
 export default class ProcessedJSCache {
-    //TODO use LRU cache instead
-
     constructor () {
-        this.items = {};
-        this.size  = 0;
-    }
+        this.cache = new Lru({
+            //NOTE: Max cache size is 50 MBytes
+            max: 50 * 1024 * 1024,
 
-    _cleanUp () {
-        var cache = this;
-
-        var lruOrdered = Object.keys(this.items)
-            //NOTE: map cache data to {digest, lastUse} pair
-            .map((digest) => ({
-                digest:  digest,
-                lastUse: cache.items[digest].lastUse
-            }))
-            //NOTE: sort it by lastUse
-            .sort((a, b) => a.lastUse > b.lastUse ? 1 : -1);
-
-        for (var i = 0; i < lruOrdered.length; i++) {
-            var digest = lruOrdered[i].digest;
-
-            cache.size -= cache.items[digest].size;
-            delete cache.items[digest];
-
-            if (cache.size <= CLEAN_UP_DEST_SIZE)
-                break;
-        }
+            length: function (n) {
+                // 1 char ~ 1 byte
+                return n.length;
+            }
+        });
     }
 
     add (js, processedJs) {
-        var cache   = this;
-        var lastUse = new Date().getTime();
+        var hash = Crypto.createHash('md5');
 
-        setTimeout(function () {
-            var hash = Crypto.createHash('md5');
+        hash.update(js);
 
-            hash.update(js);
+        var digest = hash.digest('hex');
 
-            var digest = hash.digest('hex');
-            var size   = processedJs.length;
-
-            cache.size += size;
-
-            cache.items[digest] = {
-                data:    processedJs,
-                size:    size,
-                lastUse: lastUse
-            };
-
-            if (cache.size > MAX_SIZE)
-                cache._cleanUp();
-        });
+        this.cache.set(digest, processedJs);
     }
 
     pick (js) {
@@ -66,15 +29,8 @@ export default class ProcessedJSCache {
 
         hash.update(js);
 
-        var digest    = hash.digest('hex');
-        var cacheItem = this.items[digest];
+        var digest = hash.digest('hex');
 
-        if (cacheItem) {
-            cacheItem.lastUse = new Date().getTime();
-
-            return cacheItem.data;
-        }
-
-        return null;
+        return this.cache.get(digest);
     }
 }
